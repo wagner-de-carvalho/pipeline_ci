81013f05ea8b81a971f243455e0ee564f6f69442
wagner carvalho <wagner.carvalho@credisis.com.br>
2022-03-29 12:19:24 -0300

    dialyzer

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule PipelineCi.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
-      {:sobelow, "~> 0.8", only: :dev}
+      {:sobelow, "~> 0.8", only: :dev},
+      {:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false}
     ]
   end
 
