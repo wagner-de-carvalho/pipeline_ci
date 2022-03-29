29ffaea03d7e3b9d40a633e22b98e3c742ff78d2
wagner carvalho <wagner.carvalho@credisis.com.br>
2022-03-29 11:15:50 -0300

    config coveralls

--- lib/pipeline_ci_web/router.ex
+++ lib/pipeline_ci_web/router.ex
@@ -10,9 +10,9 @@ defmodule PipelineCiWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  # pipeline :api do
+  #   plug :accepts, ["json"]
+  # end
 
   scope "/", PipelineCiWeb do
     pipe_through :browser
@@ -25,6 +25,7 @@ defmodule PipelineCiWeb.Router do
   #   pipe_through :api
   # end
 
+  # coveralls-ignore-start
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
@@ -41,6 +42,8 @@ defmodule PipelineCiWeb.Router do
       live_dashboard "/dashboard", metrics: PipelineCiWeb.Telemetry
     end
   end
+
+  # coveralls-ignore-stop
 
   # Enables the Swoosh mailbox preview in development.
   #
