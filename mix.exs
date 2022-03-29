537eac36d871e6e1b67583f4d1e79cf08d89cd8f
wagner carvalho <wagner.carvalho@credisis.com.br>
2022-03-29 11:59:03 -0300

    sobelow config

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule PipelineCi.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
